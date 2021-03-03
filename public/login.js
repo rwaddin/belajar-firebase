b67b14c38d0cf3bd99f19453da2671a396ecc0d3
addin <akhul.syaifudin@rumahweb.co.id>
2021-03-03 17:04:14 +0700

    print data login

--- public/login.js
+++ public/login.js
@@ -84,7 +84,14 @@ logout.addEventListener("click", ()=>{
 auth.onAuthStateChanged((user)=>{
 
     if(user){
-        console.warn("login");
+        console.warn("login", user.uid);
+        user.providerData.forEach(function (profile) {
+            console.log("  Sign-in provider: " + profile.providerId);
+            console.log("  Provider-specific UID: " + profile.uid);
+            console.log("  Name: " + profile.displayName);
+            console.log("  Email: " + profile.email);
+            console.log("  Photo URL: " + profile.photoURL);
+          });
 
         signup.setAttribute("hidden","");
         login.setAttribute("hidden","");
